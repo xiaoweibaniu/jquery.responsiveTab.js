d4eab74ccd6a891daa959eadc379c64d0fd49d3b
evildancer <wuyuxieying@sina.com>
2017-03-02 17:01:42 +0800

    增加示例页面

--- responsive-tabs.js
+++ responsive-tabs.js
@@ -1,213 +1,259 @@
 (function (window, document, $) {
-	"use strict";
-	
-	var pluginName = 'responsiveHorizontalTabs',
-		defaults = {
-			navSelector: '.nav-tabs',
-			itemSelector: '>li',
-			dropdownSelector: '>.dropdown',
-			dropdownItemSelector: 'li',
-			tabSelector: '.tab-pane',
-			activeClassName: 'active'
-		};
-	var Plugin = function (el, options) {
-		var $tabs = this.$tabs = $(el);
-		this.options = options = $.extend(true, {}, defaults, options);
-		
-		var $nav = this.$nav = $tabs.find(this.options.navSelector),
-			$dropdown = this.$dropdown = $nav.find(this.options.dropdownSelector);
-		var $items = this.$items = $nav.find(this.options.itemSelector).filter(function () {
-			return !$(this).is($dropdown);
-		});
-		
-		this.$dropdownItems = $dropdown.find(this.options.dropdownItemSelector);
-		this.$tabPanel = this.$tabs.find(this.options.tabSelector);
-		
-		this.breakpoints = [];
-		
-		$items.each(function () {
-			$(this).data('width', $(this).width());
-		});
-		
-		this.init();
-		this.bind();
-		
-	};
-	
-	Plugin.prototype = {
-		init: function () {
-			if (length <= 1) {   //if there is not tags.
-				//this.$dropdown.hide();
-				throw 'There should be some tags here ';
-			}
-			var dropWidth = this.dropWidth = this.$dropdown.width();
-			if (this.$dropdown.length === 0) {  //if there were no tags, then we create some tags.
-				this.flag = true;
-				
-				this.$nav.append('<li class="dropdown hidden" role="presentation">'
-					+ '<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">'
-					+ '<span class="caret"></span>Menu </a><ul class="dropdown-menu" role="menu"></ul></li>');
-				
-				this.$dropdown = this.$nav.find(this.options.dropdownSelector);
-				this.$dropdown.css("opacity", 0).removeClass("hidden"); //还需要将其隐藏起来
-				dropWidth = this.dropWidth = this.$dropdown.width();
-				this.$dropdown.addClass("hidden").css("opacity", 1);
-			}
-			
-			var length = this.itemsLenth = this.$items.length;
-			
-			this.breakpoints = [];
-			
-			for (var i = 0; i < length + 1; i++) {  //按照标签数划分宽度等级
-				if (i === 0) this.breakpoints.push(this.$items.eq(i).width() + dropWidth);
-				else if (i === (length - 1)) this.breakpoints.push(this.breakpoints[i - 1] + this.$items.eq(i).width() - dropWidth);
-				else if (i === length) this.breakpoints.push(this.breakpoints[i - 1] + dropWidth);
-				else this.breakpoints.push(this.breakpoints[i - 1] + this.$items.eq(i).width());
-			}
-			this.layout();
-		},
-		layout: function () {
-			console.log(this.breakpoints);
-			if (this.breakpoints.length <= 0) return;
-			
-			var width = this.$tabs.width(),
-				i = 0,
-				activeClassName = this.options.activeClassName,
-				panelIndex = this.$tabPanel.filter('.' + activeClassName).index();
-			
-			panelIndex = panelIndex < 0 ? 0 : panelIndex;
-			console.log(this.$tabs);
-			for (; i < this.breakpoints.length; i++) {   //有一个宽度大于导航条的宽度
-				if (this.breakpoints[i] > width) break;
-			}
-			
-			this.$items.removeClass(activeClassName);
-			this.$dropdownItems.removeClass(activeClassName);
-			this.$dropdown.removeClass(activeClassName);
-			
-			if (this.flag) {
-				if (i >= this.breakpoints.length - 1) {
-					this.$dropdown.addClass("hidden");
-					this.$items.show();
-					this.$items.eq(panelIndex).addClass(activeClassName);
-				} else {
-					
-					this.$dropdown.removeClass("hidden");
-					this.$dropdown.find("ul>li").remove();
-					for (var j = 0; j < this.itemsLenth; j++) {
-						if (j < i) {
-							this.$items.eq(j).show();
-						} else {
-							for (var v = i; v < this.itemsLenth; v++) {
-								this.$dropdown.find("ul").append(this.$items.eq(v).prop("outerHTML"));
-								this.$items.eq(v).hide();
-							}
-							this.$dropdown.find("ul>li").show();
-							break;
-						}
-					}
-					
-					if (panelIndex < i) {
-						this.$items.eq(panelIndex).addClass(activeClassName);
-					} else {
-						this.$dropdown.addClass(activeClassName);
-						this.$dropdownItems.eq(panelIndex).addClass(activeClassName);
-					}
-				}
-			} else {
-				if (i === this.breakpoints.length) {
-					this.$items.show();
-					this.$dropdown.find("ul>li:not(li:last)").remove();
-					this.$items.eq(panelIndex).addClass(activeClassName);
-				} else {
-					this.$dropdown.removeClass("hidden");
-					this.$dropdown.find("ul>li:not(li:last)").remove();
-					for (var j = 0; j < this.itemsLenth + 1; j++) {
-						if (j < i) {
-							this.$items.eq(j).show();
-						} else {
-							var v = i;
-							if (i === this.itemsLenth) v = i-1;
-							console.log(v);
-							for (; v < this.itemsLenth; v++) {
-								this.$dropdown.find("ul>li:last").before(this.$items.eq(v).prop("outerHTML"));
-								this.$items.eq(v).hide();
-							}
-							this.$dropdown.find("ul>li").show();
-							break;
-						}
-					}
-					
-					if (panelIndex < i) {
-						this.$items.eq(panelIndex).addClass(activeClassName);
-					} else {
-						this.$dropdown.addClass(activeClassName);
-						this.$dropdownItems.eq(panelIndex).addClass(activeClassName);
-					}
-				}
-			}
-			
-		},
-		
-		throttle: function (fn, interval) {
-			var _fn = fn,   //保存需要被延迟执行的函数引用
-				timer,     //定时器
-				firstTime = true;     //是否是第一次调用
-			return function () {
-				var args = arguments,
-					_self = this;
-				
-				if (firstTime) {  //如果是第一次调用，不需要延迟执行
-					_fn.apply(_self, args);
-					return firstTime = false;
-				}
-				
-				if (timer) {    //如果定时器还在，说明前一次延迟执行还没有完成
-					return false;
-				}
-				
-				timer = setInterval(function () {   //延迟一段时间执行
-					clearInterval(timer);
-					timer = null;
-					_fn.apply(_self, args);
-				}, interval || 500);
-			}
-		},
-		bind: function () {
-			var self = this;
-			
-			$(window).resize(function () {
-				self.throttle(function () {
-					self.layout();
-				}, 1000)();
-			});
-		}
-	};
-	
-	
-	$.fn[pluginName] = function (options) {
-		if (typeof options === 'string') {
-			var method = options;
-			var method_arguments = Array.prototype.slice.call(arguments, 1);
-			if (/^\_/.test(method)) {
-				return false;
-			} else {
-				return this.each(function () {
-					var api = $.data(this, pluginName);
-					if (api && typeof api[method] === 'function') {
-						api[method].apply(api, method_arguments);
-					}
-				});
-			}
-		} else {
-			return this.each(function () {
-				if (!$.data(this, pluginName)) {
-					$.data(this, pluginName, new Plugin(this, options));
-					if (typeof options === 'function') options();
-				} else {
-					$.data(this, pluginName).init();
-				}
-			});
-		}
-	};
+    "use strict";
+
+    var Plugin,
+        pluginName = 'responsiveHorizontalTabs',
+        defaults = {
+            navSelector: '.nav-tabs',
+            itemSelector: '>li',
+            dropdownSelector: '>.dropdown',
+            dropdownItemSelector: 'li',
+            tabParentSelector: '',
+            tabSelector: '.tab-pane',
+            activeClassName: 'active',
+            noNavClassName: '.no-menu',
+            closeSelector: '.close-tab',
+            closeNav: true,
+            fnCallback: ''
+        };
+    Plugin = function (el, options) {
+        var $tabs = this.$tabs = $(el),
+            opts = this.options = $.extend(true, {}, defaults, options),
+            $nav = this.$nav = $tabs.find(opts.navSelector),
+            $dropdown = this.$dropdown = $nav.find(opts.dropdownSelector);
+
+        this.$items = $nav.find(opts.itemSelector).filter(function () {
+            return !$(this).is($dropdown);
+        });
+
+        this.$dropdownItems = $dropdown.find(opts.dropdownItemSelector);
+
+        if (opts.tabParentSelector !== '')
+            this.$tabPanel = $(opts.tabParentSelector).find(opts.tabSelector);
+        else
+            this.$tabPanel = $tabs.find(opts.tabSelector);
+
+
+        this.init();
+
+        this.bind();
+    };
+
+    Plugin.prototype = {
+        init: function () {
+            var length = this.itemsLenth = this.$items.length, dropWidth;
+
+            if (length === 0) throw 'There should be some tags here ';
+
+            if (this.$dropdown.length === 0) { // 没有下拉菜单
+                this.flag = true;
+
+                this.$nav.append('<li class="dropdown" role="presentation">'
+                    + '<a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">'
+                    + '<span class="caret"></span>Menu </a><ul class="dropdown-menu" role="menu"></ul></li>');
+
+                this.$dropdown = this.$nav.find(this.options.dropdownSelector);
+                this.$dropdown.css("opacity", 0); // 还需要将其隐藏起来
+                dropWidth = this.$dropdown.width();
+                dropWidth = dropWidth === 0 ? 90 : dropWidth;
+                this.$dropdown.addClass("hidden").css("opacity", 1);
+            } else {
+                dropWidth = this.$dropdown.width();
+            }
+
+            this.breakpoints = [];
+            for (var i = 0; i < length + 1; i++) {  // 按照标签数划分宽度等级
+                var itemWidth = this.$items.eq(i).width(), widthLevel = 0;
+
+                switch (i) {
+                    case 0:
+                        widthLevel = itemWidth + dropWidth;
+                        break;
+                    case length - 1:
+                        widthLevel = this.breakpoints[i - 1] + itemWidth - dropWidth;
+                        break;
+                    case length:
+                        widthLevel = this.breakpoints[i - 1] + dropWidth;
+                        break;
+                    default:
+                        widthLevel = this.breakpoints[i - 1] + itemWidth;
+                }
+                this.breakpoints.push(widthLevel);
+            }
+
+            if (typeof this.options.fnCallback === 'function') this.options.fnCallback(this.$tabs);
+
+            this.layout();
+            if (this.options.closeNav) this.close();
+        },
+        layout: function () {
+            if (this.breakpoints.length <= 0) return;
+
+            var width = this.$tabs.width(),
+                i = 0,
+                activeClassName = this.options.activeClassName,
+                panelIndex = this.$tabPanel.filter('.' + activeClassName).index();
+
+            for (; i < this.breakpoints.length; i++) { // 有一个宽度大于导航条的宽度
+                if (this.breakpoints[i] > width) break;
+            }
+
+            this.$items.removeClass(activeClassName);
+            this.$dropdownItems.removeClass(activeClassName);
+            this.$dropdown.removeClass(activeClassName);
+
+            if (i === this.breakpoints.length) {
+                if(this.flag)
+                    this.$dropdown.addClass("hidden");
+                else
+                    this.$dropdown.find('ul>li:not(li'+this.options.noNavClassName+')').remove();
+                this.$items.show();
+                this.$items.eq(panelIndex).addClass(activeClassName);
+            } else {
+                this.$dropdown.removeClass("hidden");
+                if(this.flag)
+                    this.$dropdown.find("ul>li").remove();
+                else
+                    this.$dropdown.find('ul>li:not(li'+this.options.noNavClassName+')').remove();
+
+                for (var j = 0; j < this.itemsLenth + 1; j++) {
+                    if (j < i) {
+                        this.$items.eq(j).show();
+                    } else {
+                        var v = i;
+                        if (i === this.itemsLenth) v = i - 1;
+                        for (; v < this.itemsLenth; v++) {
+                            if(this.flag)
+                                this.$dropdown.find("ul").append(this.$items.eq(v).prop("outerHTML"));
+                            else
+                                this.$dropdown.find('ul>li'+this.options.noNavClassName+':first')
+                                    .before(this.$items.eq(v).prop("outerHTML"));
+
+                            this.$items.eq(v).hide();
+                        }
+                        this.$dropdown.find("ul>li").show();
+                        break;
+                    }
+                }
+
+                if (panelIndex < i) {
+                    this.$items.eq(panelIndex).addClass(activeClassName);
+                } else {
+                    this.$dropdown.addClass(activeClassName);
+                    this.$dropdownItems.eq(panelIndex).addClass(activeClassName);
+                }
+            }
+        },
+        close: function () {
+            var self = this;
+            // 标签可关闭项
+            this.$tabs.on('click', this.options.closeSelector, closeFun);
+
+            function closeFun(e) {
+                var $this = $(this),
+                    $toggle = $this.closest('[data-toggle="tab"]'),
+                    selector = $toggle.data('target'),
+                    $li = $toggle.parent('li');
+
+                if (!selector) {
+                    selector = $toggle.attr('href');
+                    selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '');
+                }
+
+                if ($li.hasClass('active')) {
+                    var $next = $li.siblings().eq(0).children('[data-toggle="tab"]');
+                    if ($next.length > 0) {
+                        var api = $next.tab().data('bs.tab');
+                        api.show();
+                    }
+                }
+
+                var $parent = $(selector);
+                if (e) e.preventDefault();
+
+                $parent.trigger(e = $.Event('close.bs.tab'));
+
+                if (e.isDefaultPrevented()) return;
+
+                $parent.removeClass('in');
+
+                function removeElement() {
+                    // detach from parent, fire event then clean up data
+                    $parent.detach().trigger('closed.bs.tab').remove();
+                    $li.detach().remove();
+                    refresh();
+                    self.init();
+                }
+                function refresh(){
+                    self.$dropdown.find("ul>li:first").remove();
+
+                    if(self.$dropdown.find('ul>li').length === 0); self.$dropdown.remove();
+                }
+
+                $.support.transition && $parent.hasClass('fade') ?
+                    $parent
+                        .one('bsTransitionEnd', removeElement)
+                        .emulateTransitionEnd(150) :
+                    removeElement()
+            }
+        },
+        throttle: function (fn, interval) {
+            var _fn = fn,
+                timer,
+                firstTime = true;
+            return function () {
+                var args = arguments,
+                    _self = this;
+
+                if (firstTime) {
+                    _fn.apply(_self, args);
+                    return firstTime = false;
+                }
+
+                if (timer) {
+                    return false;
+                }
+
+                timer = setInterval(function () {
+                    clearInterval(timer);
+                    timer = null;
+                    _fn.apply(_self, args);
+                }, interval || 500);
+            }
+        },
+        bind: function () {
+            var self = this;
+
+            $(window).resize(function () {
+                self.throttle(function () {
+                    self.layout();
+                }, 1000)();
+            });
+        }
+    };
+
+    $.fn[pluginName] = function (options) {
+        if (typeof options === 'string') {
+            var method = options,
+                method_arguments = Array.prototype.slice.call(arguments, 1);
+            if (/^_/.test(method)) {
+                console.error('No such method : ' + options);
+            } else {
+                return this.each(function () {
+                    var api = $.data(this, pluginName);
+                    if (api && typeof api[method] === 'function') {
+                        api[method].apply(api, method_arguments);
+                    }
+                });
+            }
+        } else {
+            return this.each(function () {
+                if (!$.data(this, pluginName)) {
+                    $.data(this, pluginName, new Plugin(this, options));
+                } else {
+                    $.data(this, pluginName).init();
+                }
+            });
+        }
+    };
 })(window, document, jQuery);
